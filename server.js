4a74ad8403a05a24bf1123e75c8130c110567529
edidiongaligbe <edidiong.aligbe@gmail.com>
2021-09-20 08:45:08 +0100

    update

--- server.js
+++ server.js
@@ -1,61 +1,55 @@
-const express = require('express');
+const flash = require('connect-flash');
+const passport = require('passport');
+const express = require("express");
+const session = require('express-session');
 const app = new express();
 
-app.use(express.json());
+
+//passport config:
+require('./src/utils/passport')(passport)
+
+//ejs
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+//app.use(expressEjsLayout);
+
+//Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-
-//static files
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+//express session
+app.use(session({
+  secret : 'secret',
+  resave : true,
+  saveUninitialized : true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req,res,next)=> {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+    })
 
 
-//Templating Engine
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
 
-//database config file
-const db = require('./src/utils/database');
-db.sequelize.sync().then( ()=> {
-    console.log('"Drop and Resync with { force: true }"');
+
+
+const db = require("./src/utils/database");
+db.sequelize.sync().then(() => {
+  console.log('"Drop and Resync with { force: true }"');
 });
 
 //Routes
-require('./src/routes/indexroutes')(app);
-require('./src/routes/usersroutes')(app);
+require("./src/routes/indexroutes")(app);
+require("./src/routes/usersroutes")(app);
 
-port = process.env.PORT || 3000
-var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-  
-    console.log(`App listening on http://localhost:${port}`);
-  });
+//look for ways to handle errors
 
+port = process.env.PORT || 3000;
+var server = app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
 
-
-
-
-/* const express = require('express');
-const app = new express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//static files
-app.use(express.static(__dirname + "/public"));
-
-
-
-//Templating Engine
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
-
-//Routes
-app.use('/', require('./src/routes/index'));
-app.use('/users', require('./src/routes/usersroutes')); 
-
-
-
-port = process.env.PORT || 3000
-app.listen(port, () =>{
-    console.log(`App listening on http://localhost:${port}`)
-}); */
